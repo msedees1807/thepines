82c841322c6bccc56610a9e1a6872f58bad75b43
Marc Edees <Marc@marcs-mbp-4.home>
2020-05-02 11:37:17 +0100

    updates

--- src/components/menu/closeButton.js
+++ src/components/menu/closeButton.js
@@ -2,10 +2,21 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { FaTimes } from 'react-icons/fa'
 
+const styles = {
+  Fab: {
+    float: 'right',
+    margin: '5%',
+    bottom: '9vh',
+    position: 'fixed',
+    right: '2vw',
+    zIndex: 1000,
+  },
+}
+
 export default class CloseButton extends React.Component {
   render() {
     return (
-      <div>
+      <div style={styles.Fab}>
         <Link to="./">
           <FaTimes className="closeButton" size={56} />
         </Link>
