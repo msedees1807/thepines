2f4115800e64b2e99fd2349f4b1232e5ecfbd7fc
Marc Edees <Marc@marcs-mbp-4.home>
2020-07-06 10:10:26 +0100

    fixed menu button bug

--- src/components/menu/closeButton.js
+++ src/components/menu/closeButton.js
@@ -17,7 +17,7 @@ export default class CloseButton extends React.Component {
   render() {
     return (
       <div style={styles.Fab}>
-        <Link to="./">
+        <Link to="/">
           <FaTimes className="closeButton" size={56} />
         </Link>
       </div>
